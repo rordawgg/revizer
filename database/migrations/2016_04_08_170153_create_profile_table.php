bbdc9b1b053a82160148b03fcd22be5518589474
ace2323 <tonyhatch2323@gmail.com>
2016-04-08 10:39:25 -0700

    Test

--- database/migrations/2016_04_08_170153_create_profile_table.php
+++ database/migrations/2016_04_08_170153_create_profile_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProfileTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('profiles', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->integer('user_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('avatar');
+            $table->string('username');
+            $table->timestamps('created_at');
         });
     }
 
