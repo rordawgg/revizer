256a34de780c8d6a5a804f87ed4aa6a720f6959c
ace2323 <tonyhatch2323@gmail.com>
2016-04-11 13:21:10 -0700

    Create Layout view

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+        <head>
+            <meta charset="UTF-8">
+            
+            <title>@yield('title')</title>
+        </head>
+
+
+
+            <body>
+                	
+                	@yield('content')
+                
+                	@yield('footer')
+                	
+            </body>
+
+</html>
