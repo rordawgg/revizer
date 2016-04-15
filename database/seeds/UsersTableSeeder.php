083d4cf89b5da2aa25a8c8413b0c67bb4a05ff4e
Chris H <crhowell1@gmail.com>
2016-04-15 12:25:12 -0700

    Add a seeded admin user for testing.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,6 +16,15 @@ class UsersTableSeeder extends Seeder
          * With every pass over a user it creates a new Doc and attaches
          * a random id to the user_id field in the Doc entry. 
          */
+
+        factory(App\User::class, 1)->create([
+                'name' => 'Admin',
+                'email' => 'admin@example.com',
+                'password' => bcrypt('admin')
+            ])->each(function($u){
+            factory(App\Doc::class, 5)->create(['user_id' => $u->id]);
+        });
+
         $numUsers = 20; 
     	factory(App\User::class, $numUsers)->create()->each(function($u, $numUsers){
     		factory(App\Doc::class)->create(['user_id' => rand(1, $numUsers)]);
