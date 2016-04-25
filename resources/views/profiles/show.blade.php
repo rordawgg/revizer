0ba8c94fb43476acef28447a2af23da634e9dc85
Max Pennington <your_email@example.com>
2016-04-25 14:30:27 -0700

    Add edit button for profile page

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,6 +1,12 @@
 @extends("layout")
 
 @section('content')
+
+@if($profile->user_id === Auth::user()->id)
+    <div>
+        <a href="/user/me/edit">Edit</a>
+    </div>
+@endif
 <h1>{{ $profile->username  }}</h1>
 <img src="http://www.gravatar.com/avatar/{{ $profile->avatar }}?d=identicon&s=200" alt="">
 
