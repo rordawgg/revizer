8ee69b53e5fadaadb37496fee369d3f57380e5c0
Max Pennington <your_email@example.com>
2016-04-27 11:42:38 -0700

    Add revision model and controller

---
+++ app/Http/Controllers/RevsionsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class RevsionsController extends Controller
+{
+    public function create(Doc $doc)
+    {
+    	return view("revisions.create")->with("docId", $doc->id);
+    }
+}
