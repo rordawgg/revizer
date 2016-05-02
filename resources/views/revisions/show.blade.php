8988a8dc012aede933763058b1866c687bf7b6f8
Chris H <crhowell1@gmail.com>
2016-05-02 14:21:51 -0700

    Change revision show page to use auth check

--- resources/views/revisions/show.blade.php
+++ resources/views/revisions/show.blade.php
@@ -5,15 +5,16 @@
 @section("content")
 	<h2>Revision of: {{ $doc->title }}</h2>
 	<hr/>
-@if(request()->user() && ($doc->user_id === request()->user()->id))
+@if(Auth::check() && $doc->user_id === Auth::user()->id)
 	<form method="post" action="{{ action("RevisionsController@revise", ["doc" => $doc->id, "revision" => $revision->id]) }}">
 				<input type="hidden" name="_token" value="{{ csrf_token() }}">
 				<input type="hidden" name="_method" value="patch">
 				<button	type="submit">Accept Revision</button>
 	</form>
 @endif
-	{{ $revision->description }}<br>
-	{{ $revision->body }}
+	<h4>Description: {{ $revision->description }}</h4>
+
+	<p>{{ $revision->body }}</p>
 
 
 
