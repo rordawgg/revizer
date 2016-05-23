be1e07f9ead73fce028bf155c77a1260ce0017b9
Tony Hatch <tonyhatch2323@gmail.com>
2016-05-23 13:55:52 -0700

    Update Small Navigation

--- resources/views/partial/small_nav.blade.php
+++ resources/views/partial/small_nav.blade.php
@@ -1,15 +1,23 @@
 <nav id="sm-nav">
 	<ul>
-		<li>
-			<a id="nav-menu" href="">
+		<li id="nav-menu-cont">
+			<a id="nav-menu-btn" href="">
 				<div></div>
 			</a>
 		</li>
 		<li>
-			<a href=""></a>
+			<a id="search-menu-btn" href="{{ url('/search') }}">
+				<img src={{ asset("img/icons/search.svg") }} alt="">
+			</a>
 		</li>
-		<li>
-			<a href=""></a>
-		</li>
+		@if(Auth::check())
+			<li>
+				<a id="grav-menu-btn" href="{{ url("/user/me") }}">
+					<div>
+					<img src="http://www.gravatar.com/avatar/{{ Auth::user()->profile->avatar }}?d=identicon&s=200" alt="">
+					</div>
+				</a>
+			</li>
+		@endif
 	</ul>
 </nav>
