80c899fd7dd58c8260ff0f513930a767674b8429
Tony Hatch <tonyhatch2323@gmail.com>
2016-05-27 10:27:32 -0700

    Stop scrolling while on Search

--- public/js/app.js
+++ public/js/app.js
@@ -9,6 +9,14 @@ $(document).ready(function () {
 		return showSearch(event);
 	});
 
+	$('body').on({
+	    'mousewheel': function(e) {
+	        if (e.target.id != 'full-search-cont') return;
+	        e.preventDefault();
+	        e.stopPropagation();
+	    }
+	})
+
 	function showSearch(event) {
 		event.preventDefault();
 		$("#full-search-cont").fadeToggle(200);
