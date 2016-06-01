31fc32a0949a005276b5b1dae0c6bcfb016e2f8e
Tony Hatch <tonyhatch2323@gmail.com>
2016-06-01 15:01:50 -0700

    Finish show view

--- resources/views/docs/show.blade.php
+++ resources/views/docs/show.blade.php
@@ -5,33 +5,46 @@
 @section("content")
 
 	<div id="doc-cont">
-		@if(!empty(Auth::user()->id) && ($doc->user_id === Auth::user()->id))
-		    <div>
-		        <a href="{{ url('/doc/' . $doc->id . '/edit') }}">Edit</a>
-		    </div>
-		@endif
+		<div class="author-info">
+			<img src="http://www.gravatar.com/avatar/{{ $doc->user->profile->avatar }}?d=identicon&s=50" alt="">
+			<span>
+				{{ $doc->user->profile->username }} - {{ date("m/d/Y", time($doc->updated_at)) }}
+				@if(!empty(Auth::user()->id) && ($doc->user_id === Auth::user()->id))
+				    <div>
+				        <a href="{{ url('/doc/' . $doc->id . '/edit') }}">Edit Document</a>
+				    </div>
+				@endif
+			</span>
+		</div>
+
 		<header>
 			<h1>{{ $doc->title }}</h1>
 		</header>
-
+		
 		<div class="category">
-			<h2>Category</h2>
-			<p>{{ ucfirst($doc->cat->name) }}</p>
+			<div>{{ ucfirst($doc->cat->name) }}</div>
 		</div>
 
-		<div class="description">
-			<h2>Description</h2>
-			<p>{{ $doc->description }}</p>
-		</div>
+		<div class="doc-info">
+			<hr>
+			<div class="des-crit-cont">
+				<div class="description">
+					<div><h5>Description</h5></div>
+					<p>{{ $doc->description }}</p>
+				</div>
 
-		<div class="criteria">
-			<h2>Criteria</h2>
-			<p>{{ $doc->criteria }}</p>
+				<div class="criteria">
+					<div><h5>Criteria</h5></div>
+					<p>{{ $doc->criteria }}</p>
+				</div>
+			</div>
+			<hr>
 		</div>
 
 		<div class="body">
-			<h2>Body</h2>
-			<p>{{ $doc->body }}</p>
+			<h3>Body</h3>
+			<p><pre>{{ $doc->body }}</pre></p>
+			<hr>
 		</div>
 	</div>
 	
