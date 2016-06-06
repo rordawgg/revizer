d8b0b5fb248172e5aefd1f8043fa5a7cdedd8dc4
Max Pennington <your_email@example.com>
2016-06-06 15:00:39 -0700

    Add media queries

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href={{ asset("css/lib/normalize.css") }}>
         <link rel="stylesheet" href={{ asset("css/lib/skeleton.css") }}>
         <link rel="stylesheet" href={{ asset("css/app.css") }}>
