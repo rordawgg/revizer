d76cde5dc64e1213ee7f424f378b414e5636d6f6
rordawgg <rory.dwy@gmail.com>
2016-06-08 10:11:27 -0700

    Add contact us to footer

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -1,1 +1,6 @@
-<div class="copyright">Copyright &copy; <?php echo date("Y") ?> TheRevizrTeam</div>
+<div class="copyright">
+
+<p>Contact us at revizrme@gmail.com   |   Copyright &copy; <?php echo date("Y") ?> TheRevizrTeam</p>
+
+
+</div>
